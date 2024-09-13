113393668eccf17adf40b86495858fbf8f3004b9
Aaron Li <aaronlee916@qq.com>
2024-09-13 13:01:31 +0800

    新增artistdetail页

---
+++ lib/pages/artistDetail.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class artistDetail extends StatefulWidget {
+  const artistDetail({super.key});
+
+  @override
+  State<artistDetail> createState() => _artistDetailState();
+}
+
+class _artistDetailState extends State<artistDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          
+        ],
+      ),
+    )
+  }
+}
